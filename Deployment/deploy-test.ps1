fafa10490eb4da8320a464a4cd3f62e21594538b
Johan Leino <leino@outlook.com>
2013-03-05 20:12:59 +0100

    now takes parameter

--- Deployment/deploy-test.ps1
+++ Deployment/deploy-test.ps1
@@ -1,10 +1,15 @@
 Add-PSSnapin WDeploySnapin3.0
 
+param 
+(
+    [string]$PackagePath = $(throw '- Need parameter package file path')
+)
+
 $0 = $MyInvocation.MyCommand.Definition
 $dp0 = [System.IO.Path]::GetDirectoryName($0)
 
 $params = Get-WDParameters -FilePath "$dp0\test.example.com.xml"
 
-Restore-WDSite -Parameters $params -Package "$dp0\Example.Web.zip" -DestinationPublishSettings "$dp0\wfe1.publishsettings"
+Restore-WDSite -Parameters $params -Package $PackagePath -DestinationPublishSettings "$dp0\wfe1.publishsettings"
 
 Write-Host "site deployed"
