7666074b7a82928bc12214c658bd11a1cdaa22f4
Johan Leino <leino@outlook.com>
2013-03-05 13:34:07 +0100

    updated

---
+++ Deployment/deploy-test.ps1
@@ -0,0 +1,8 @@
+Add-PSSnapin WDeploySnapin3.0
+
+$0 = $MyInvocation.MyCommand.Definition
+$dp0 = [System.IO.Path]::GetDirectoryName($0)
+
+$params = Get-WDParameters -FilePath "$dp0\test.example.com.xml"
+
+Restore-WDSite -Parameters $params -Package "$dp0\Example.Web.zip" -DestinationPublishSettings "$dp0\wfe1.publishsettings"
