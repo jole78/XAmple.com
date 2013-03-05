e250fd40750a84906af8d1a28e5563aa23ca5067
Johan Leino <leino@outlook.com>
2013-03-05 19:57:42 +0100

    updated powershell script

--- Deployment/deploy-test.ps1
+++ Deployment/deploy-test.ps1
@@ -6,3 +6,5 @@ $dp0 = [System.IO.Path]::GetDirectoryName($0)
 $params = Get-WDParameters -FilePath "$dp0\test.example.com.xml"
 
 Restore-WDSite -Parameters $params -Package "$dp0\Example.Web.zip" -DestinationPublishSettings "$dp0\wfe1.publishsettings"
+
+Write-Host "site deployed"
