6fe10455597282182be95328020232df8d3b503f
Johan Leino <leino@outlook.com>
2013-04-03 13:11:22 +0200

    added simple xunit test

---
+++ Tests.Example.Web/MyTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Tests.Example.Web
+{
+    public class MyTests
+    {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
