411b6064fedcba463c79e762e760cbe8a07bffb0
Johan Leino <leino@outlook.com>
2013-04-18 13:57:50 +0200

    added deploy.exe

---
+++ Deployment/Example.Deploy/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Example.Deploy
+{
+    class Program
+    {
+        static ExecutionContext m_ExecutionContext = ExecutionContext.Unknown;
+
+        static Program()
+        {
+            DetermineExecutionContext();
+        }
+
+        private static void DetermineExecutionContext()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TEAMCITY_DATA_PATH", EnvironmentVariableTarget.Machine)))
+                {
+                    m_ExecutionContext = ExecutionContext.Windows;
+                }
+                else
+                {
+                    m_ExecutionContext = ExecutionContext.TeamCity;
+                }
+            }
+            catch
+            {
+                m_ExecutionContext = ExecutionContext.Windows;
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            if (m_ExecutionContext == ExecutionContext.TeamCity)
+            {
+                Console.WriteLine("Executing in TeamCity");
+            }
+            else
+            {
+                Console.WriteLine("Executing in a Windows Console");
+            }
+        }
+
+    }
+
+    internal enum ExecutionContext
+    {
+        Unknown = 0,
+        Windows = 1,
+        TeamCity = 2
+    }
+}
