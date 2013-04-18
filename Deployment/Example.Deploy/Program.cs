c2ca619639c2214a23fee91996127008dfc146df
Johan Leino <leino@outlook.com>
2013-04-18 23:28:27 +0200

    refactored .exe to use delegates

--- Deployment/Example.Deploy/Program.cs
+++ Deployment/Example.Deploy/Program.cs
@@ -1,73 +1,54 @@
 ﻿using System;
-using System.Threading;
 
 namespace Example.Deploy
 {
     class Program
     {
-        static ExecutionContext m_ExecutionContext = ExecutionContext.Unknown;
 
         static Program()
         {
-            DetermineExecutionContext();
-        }
-
-        private static void DetermineExecutionContext()
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("TEAMCITY_DATA_PATH", EnvironmentVariableTarget.Machine)))
-                {
-                    m_ExecutionContext = ExecutionContext.Windows;
-                }
-                else
-                {
-                    m_ExecutionContext = ExecutionContext.TeamCity;
-                }
-            }
-            catch
-            {
-                m_ExecutionContext = ExecutionContext.Windows;
-            }
-
-
+            ConfigureExecutionContext();
         }
 
         static void Main(string[] args)
         {
+            var workflow = new DeploymentWorkflow();
+            workflow.Execute();
+        }
 
-            if (m_ExecutionContext == ExecutionContext.TeamCity)
+        private static void ConfigureExecutionContext()
+        {
+            try
             {
-                Console.WriteLine("##teamcity[progressStart 'deployment in progress...']");
+                var value = Environment.GetEnvironmentVariable("TEAMCITY_DATA_PATH",
+                                                                  EnvironmentVariableTarget.Machine);
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    DeploymentWorkflow.OnDeploymentStarting = delegate
+                                                              {
+                                                                  Console.WriteLine("deployment in progress...");
+                                                              };
+                    DeploymentWorkflow.OnDeploymentFinished = delegate
+                                                              {
+                                                                  Console.WriteLine("deployment finished successfully");
+                                                                  Console.Read();
+                                                              };
+                }
+                else
+                {
+                    DeploymentWorkflow.OnDeploymentStarting = delegate
+                    {
+                        Console.WriteLine("##teamcity[progressStart 'deployment in progress...']");
+                    };
+                    DeploymentWorkflow.OnDeploymentFinished = delegate
+                    {
+                        Console.WriteLine("##teamcity[progressFinish 'deployment in progress...']");
+                    };
+                }
             }
-            else
-            {
-                Console.WriteLine("deployment in progress...");
-            }
-            
-            // START deployment
-            // simulated by pausing for 10 seconds...
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-
-            if (m_ExecutionContext == ExecutionContext.TeamCity)
-            {
-                Console.WriteLine("##teamcity[progressFinish 'deployment completed successfully']");
-            }
-            else
-            {
-                Console.WriteLine("deployment completed successfully");
-                Console.Read();
-            }
-
-
+            catch { }
         }
 
     }
 
-    internal enum ExecutionContext
-    {
-        Unknown = 0,
-        Windows = 1,
-        TeamCity = 2
-    }
 }
