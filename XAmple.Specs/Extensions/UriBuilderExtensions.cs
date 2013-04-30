923f24df34f7f2a749145c6e93dfddce32ffcc26
Johan Leino <leino@outlook.com>
2013-04-30 14:42:45 +0200

    implemented the AddQueryString method

--- XAmple.Specs/Extensions/UriBuilderExtensions.cs
+++ XAmple.Specs/Extensions/UriBuilderExtensions.cs
@@ -1,16 +1,19 @@
-﻿namespace System
+﻿// ReSharper disable CheckNamespace
+namespace System
+// ReSharper restore CheckNamespace
 {
     public static class UriBuilderExtensions
     {
          public static void AddQueryString(this UriBuilder instance, string key, string value)
          {
-             //UriBuilder baseUri = new UriBuilder("http://www.contoso.com/default.aspx?Param1=7890");
-             //string queryToAppend = "param2=1234";
-
-             //if (baseUri.Query != null && baseUri.Query.Length > 1)
-             //    baseUri.Query = baseUri.Query.Substring(1) + "&" + queryToAppend;
-             //else
-             //    baseUri.Query = queryToAppend; 
+             if (instance.Query != null && instance.Query.Length > 1)
+             {
+                 instance.Query = string.Format("{0}&{1}={2}", instance.Query.Substring(1), key, value);
+             }
+             else
+             {
+                 instance.Query = string.Format("{0}={1}", key, value);
+             }
          }
     }
 }
