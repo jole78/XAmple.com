b9cee9b720951809b9f7272c84d1d5e4b39c014b
Johan Leino <leino@outlook.com>
2013-05-05 21:49:43 +0200

    Added stateless authentication

---
+++ XAmple.Web/ApiUser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Nancy.Security;
+
+namespace XAmple.Web
+{
+    public class ApiUser : IUserIdentity
+    {
+        public string UserName { get; set; }
+        public IEnumerable<string> Claims { get; set; }
+
+        public ApiUser()
+        {
+            UserName = "Api User";
+            Claims = new string[] { "ApiUser" };
+        }
+
+    }
+}
