34042ff4dd6fc91cc7dea77f628c2c42664fbeb0
Johan Leino <leino@outlook.com>
2013-05-05 22:32:28 +0200

    updated packages to fix broken tests

--- XAmple.Web.Tests/VersionModuleTests.cs
+++ XAmple.Web.Tests/VersionModuleTests.cs
@@ -1,35 +1,19 @@
-﻿using System;
-using FluentAssertions;
-using Moq;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using Nancy;
-using Nancy.Security;
 using Nancy.Testing;
-using XAmple.Web.Modules;
-using XAmple.Web.Services;
 
 namespace XAmple.Web.Tests
 {
     [TestFixture]
     public class VersionModuleTests
     {
-        // TODO: investigate why ApiUser object causes error
 
         [Test]
-        public void TEST()
+        public void Get_WithoutApiKey_ShouldResultInUnauthorizedResponse()
         {
             // GIVEN
-            var versionService = Mock.Of<IVersionService>();
-
-            var browser = new Browser(cfg =>
-                                      {
-                                          cfg.Module<VersionModule>();
-                                          cfg.Dependency<IVersionService>(versionService);
-                                      });
-
-            Mock.Get(versionService)
-                .Setup(x => x.GetVersionInformation())
-                .Returns(new Version("1.0.0.1"));
+            var browser = new Browser(new Bootstrapper());
 
             // WHEN
             var response = browser.Get("/about/version");
@@ -44,27 +28,16 @@ namespace XAmple.Web.Tests
         }
 
         [Test]
-        public void TEST2()
+        public void Get_WithApiKey_ShouldResultInOKResponse()
         {
             // GIVEN
-            var versionService = Mock.Of<IVersionService>();
-
-            var browser = new Browser(cfg =>
-            {
-                cfg.Module<VersionModule>();
-                cfg.Dependency<IVersionService>(versionService);
-            });
-
-            Mock.Get(versionService)
-                .Setup(x => x.GetVersionInformation())
-                .Returns(new Version("1.0.0.1"));
+            var browser = new Browser(new Bootstrapper());
 
             // WHEN
             var response = browser.Get("/about/version", x =>
                                                          {
                                                              x.Query("ApiKey","pass@word1");
                                                          });
-
             // THEN
             response
                 .StatusCode
